6981a273f5a41eb46b245bda109fc96ee1c4bf14
sergiobsilva2505 <sergio.bezerra@outlook.com>
2021-10-03 01:25:24 -0300

    ajustes em index.js

--- client/app/index.js
+++ client/app/index.js
@@ -1,31 +1,21 @@
 
-var fields = [
+let fields = [
     document.querySelector('#artist'),
     document.querySelector('#albumTitle'),
     document.querySelector('#releaseDateOf'),
     document.querySelector('#recordCompany')
 ];
 
-console.log(fields);
-
-var tbody = document.querySelector('table tbody');
+let tbody = document.querySelector('table tbody');
 
 document.querySelector('.form').addEventListener('submit',function (event) {
 
     event.preventDefault();
 
-    var tr = document.createElement('tr');
-
-    var tdEditLink = document.createElement('a');
-    tdEditLink.textContent = 'Editar'
-    tdEditLink.href = 'https://www.google.com.br/';
-
-    var btnDeleteLink = document.createElement('button');
-    btnDeleteLink.textContent = 'Deletar'
-    btnDeleteLink.href = 'https://br.yahoo.com/';
+    let tr = document.createElement('tr');
 
     fields.forEach(function (field) {
-        var td = document.createElement('td');
+        let td = document.createElement('td');
         td.textContent = field.value;
         tr.appendChild(td);
     });
