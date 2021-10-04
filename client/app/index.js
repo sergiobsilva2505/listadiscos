4c95f0196c9142a1a0e3ba30da619efc9b453209
sergiobsilva2505 <sergio.bezerra@outlook.com>
2021-10-04 14:52:36 -0300

    ajustando links na tabela

--- client/app/index.js
+++ client/app/index.js
@@ -14,12 +14,27 @@ document.querySelector('.form').addEventListener('submit',function (event) {
 
     let tr = document.createElement('tr');
 
+    let tdVolume = document.createElement('td');
+    tdVolume.innerHTML = '<a id="editLink" class="text-center" type="button">Editar</a>';
+
+    let tdVolume2 = document.createElement('td');
+    tdVolume2.innerHTML = '<a id="deleteLink" class="text-center" type="button">Deletar</a>';
+
     fields.forEach(function (field) {
         let td = document.createElement('td');
         td.textContent = field.value;
         tr.appendChild(td);
+        tr.appendChild(tdVolume);
+        tr.appendChild(tdVolume2);
     });
 
     tbody.appendChild(tr);
+
+    fields[0].value = '';
+    fields[1].value = '';
+    fields[2].value = '';
+    fields[3].value = '';
+
+    fields[0].focus();
 
 });
